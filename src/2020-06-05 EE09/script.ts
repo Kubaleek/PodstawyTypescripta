918c3e69fe293a4b425623885706315397d2013e
Kuba <kuba.krol.ldz@gmail.com>
2024-10-29 18:35:18 +0100

    .

--- src/2020-06-05 EE09/script.ts
+++ src/2020-06-05 EE09/script.ts
@@ -1,13 +1,9 @@
 const HandlerSubmit = (e: Event) => {
     e.preventDefault();
 
-    // Pobranie wszystkich elementów radio
-    const radioInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type="radio"]');
-    let suma = 0;
+    const selectedRadio = document.querySelector<HTMLInputElement>('input[type="radio"]:checked');
     const result = document.getElementById('costResult') as HTMLParagraphElement;
-    const selectedRadio = Array.from(radioInputs).find((i) => i.checked);
-    if (selectedRadio){
-        suma = parseInt(selectedRadio.value) || 0;
-        result.innerHTML = `Cena zabiegów: ${suma} zł`;
-    }
+    
+    const suma = selectedRadio ? parseInt(selectedRadio.value) || 0 : 0;
+    result.innerHTML = `Cena zabiegów: ${suma} zł`;
 }
